a03d5a6e6c2b725302855ca6de43201e54c1da48
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-03-14 21:43:41 +0200

    Configure services in Applicarion layer and define pipelines

---
+++ Domain-Driven Architecture Advanced/Blog/Blog.Application/Common/Behaviours/RequestPerformanceBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using Blog.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Blog.Application.Common.Behaviours
+{
+    public class RequestPerformanceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly Stopwatch timer;
+        private readonly ILogger<TRequest> logger;
+        private readonly ICurrentUser currentUserService;
+        private readonly IIdentity identityService;
+
+        public RequestPerformanceBehaviour(
+            ILogger<TRequest> logger,
+            ICurrentUser currentUserService,
+            IIdentity identityService)
+        {
+            this.timer = new Stopwatch();
+
+            this.logger = logger;
+            this.currentUserService = currentUserService;
+            this.identityService = identityService;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            this.timer.Start();
+
+            var response = await next();
+
+            this.timer.Stop();
+
+            var elapsedMilliseconds = this.timer.ElapsedMilliseconds;
+
+            if (elapsedMilliseconds <= 500)
+            {
+                return response;
+            }
+
+            var requestName = typeof(TRequest).Name;
+            var userId = this.currentUserService.UserId;
+            var userName = await this.identityService.GetUserName(userId);
+
+            this.logger.LogWarning(
+                "Blog Long Running Request: {Name} ({ElapsedMilliseconds} milliseconds) {@UserId} {@Request}",
+                requestName,
+                elapsedMilliseconds,
+                userId,
+                userName ?? "Anonymous",
+                request);
+
+            return response;
+        }
+    }
+}
---
+++ Domain-Driven Architecture Advanced/Blog/Blog.Application/ServiceRegistration.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Blog.Application.Common.Behaviours;
+using Blog.Application.Common.Services;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Reflection;
+
+namespace Blog.Application
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services
+                .AddAutoMapper(Assembly.GetExecutingAssembly())
+                .AddMediatR(Assembly.GetExecutingAssembly())
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPerformanceBehaviour<,>))
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+
+            return services;
+        }
+
+        public static IServiceCollection AddConventionalServices(
+            this IServiceCollection services,
+            Assembly assembly)
+        {
+            var transientServiceInterfaceType = typeof(ITransientService);
+            var singletonServiceInterfaceType = typeof(ISingletonService);
+            var scopedServiceInterfaceType = typeof(IScopedService);
+
+            var types = assembly
+                .GetExportedTypes()
+                .Where(t => t.IsClass && !t.IsAbstract)
+                .Select(t => new
+                {
+                    Service = t.GetInterface($"I{t.Name.Replace("Service", string.Empty)}"),
+                    Implementation = t
+                })
+                .Where(t => t.Service != null);
+
+            foreach (var type in types)
+            {
+                if (transientServiceInterfaceType.IsAssignableFrom(type.Service))
+                {
+                    services.AddTransient(type.Service, type.Implementation);
+                }
+                else if (singletonServiceInterfaceType.IsAssignableFrom(type.Service))
+                {
+                    services.AddSingleton(type.Service, type.Implementation);
+                }
+                else if (scopedServiceInterfaceType.IsAssignableFrom(type.Service))
+                {
+                    services.AddScoped(type.Service, type.Implementation);
+                }
+            }
+
+            return services;
+        }
+    }
+}
