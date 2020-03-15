88b2dfcc65f3c2c7e6833cfe61b467d447eaaf74
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-03-15 14:42:07 +0200

    Blog.Domain.UnitTests

---
+++ Domain-Driven Architecture Advanced/Blog/Tests/Domain.UnitTests/Entities/ArticleTests.cs
@@ -0,0 +1,23 @@
+﻿using Blog.Domain.Entities;
+using Blog.Domain.Exceptions;
+using Xunit;
+
+namespace Blog.Domain.UnitTests.Entities
+{
+    public class ArticleTests
+    {
+        [Fact]
+        public void TitleShouldThrowExceptionWhenNull()
+        {
+            // Assert
+            Assert.Throws<InvalidEntityException>(() => new Article(null, "Test Content", "Test Id"));
+        }
+
+        [Fact]
+        public void UserIdShouldThrowExceptionWhenNull()
+        {
+            // Assert
+            Assert.Throws<InvalidEntityException>(() => new Article("Test Title", "Test Content", null));
+        }
+    }
+}
